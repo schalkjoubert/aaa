3a7f4b2acaeb6daf3c1d21251b82737a04c9030c
Schalk Joubert <schalk@thissideup.media>
2023-03-09 13:33:55 +0200

    initial

---
+++ aaa/public/partials/test-aaa-public-display.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Provide a public-facing view for the plugin
+ *
+ * This file is used to markup the public-facing aspects of the plugin.
+ *
+ * @link       https://thissideup.media
+ * @since      1.0.0
+ *
+ * @package    Test_Aaa
+ * @subpackage Test_Aaa/public/partials
+ */
+?>
+
+<!-- This file should primarily consist of HTML with a little bit of PHP. -->
